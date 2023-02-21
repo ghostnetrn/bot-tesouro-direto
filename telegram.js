43876cc1fc4b5a2ba706e1238a6f557377149875
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2023-02-21 18:10:33 -0300

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -119,21 +119,21 @@ bot.action("titulosBons", async (ctx) => {
       console.log(cotacao.rentabilidadeAnual);
       console.log(dadostesouro.median);
 
-      if (
-        cotacao.rentabilidadeAnual >= dadostesouro.median &&
-        cotacao.rentabilidadeAnual < dadostesouro.q3
-      ) {
-        message = `*Título:* ${cotacao.titulo}\n*Preço unitário:* ${cotacao.precoUnitario}\n*Investimento mínimo:* ${cotacao.investimentoMinimo}\n*Rentabilidade anual:* ${cotacao.rentabilidadeAnual}%\n*Vencimento:* ${cotacao.vencimento}\n\n`;
-        message += `*Mínimo:* ${dadostesouro.min}\n*1º quartil:* ${dadostesouro.q1}\n*Mediana:* ${dadostesouro.median}\n*3º quartil:* ${dadostesouro.q3}\n*Máximo:* ${dadostesouro.max}\n*Média:* ${dadostesouro.mean}\n*Desvio padrão:* ${dadostesouro.stdev}\n\n`;
-        message += "😗 *J3 - COMPRA BOA*\n\n";
-      } else if (
-        cotacao.rentabilidadeAnual >= dadostesouro.q3 ||
-        cotacao.rentabilidadeAnual >= dadostesouro.max
-      ) {
-        message = `*Título:* ${cotacao.titulo}\n*Preço unitário:* ${cotacao.precoUnitario}\n*Investimento mínimo:* ${cotacao.investimentoMinimo}\n*Rentabilidade anual:* ${cotacao.rentabilidadeAnual}%\n*Vencimento:* ${cotacao.vencimento}\n\n`;
-        message += `*Mínimo:* ${dadostesouro.min}\n*1º quartil:* ${dadostesouro.q1}\n*Mediana:* ${dadostesouro.median}\n*3º quartil:* ${dadostesouro.q3}\n*Máximo:* ${dadostesouro.max}\n*Média:* ${dadostesouro.mean}\n*Desvio padrão:* ${dadostesouro.stdev}\n\n`;
-        message += "😀 *J4 - COMPRA ÓTIMA*\n\n";
-      }
+      // if (
+      //   cotacao.rentabilidadeAnual >= dadostesouro.median &&
+      //   cotacao.rentabilidadeAnual < dadostesouro.q3
+      // ) {
+      //   message = `*Título:* ${cotacao.titulo}\n*Preço unitário:* ${cotacao.precoUnitario}\n*Investimento mínimo:* ${cotacao.investimentoMinimo}\n*Rentabilidade anual:* ${cotacao.rentabilidadeAnual}%\n*Vencimento:* ${cotacao.vencimento}\n\n`;
+      //   message += `*Mínimo:* ${dadostesouro.min}\n*1º quartil:* ${dadostesouro.q1}\n*Mediana:* ${dadostesouro.median}\n*3º quartil:* ${dadostesouro.q3}\n*Máximo:* ${dadostesouro.max}\n*Média:* ${dadostesouro.mean}\n*Desvio padrão:* ${dadostesouro.stdev}\n\n`;
+      //   message += "😗 *J3 - COMPRA BOA*\n\n";
+      // } else if (
+      //   cotacao.rentabilidadeAnual >= dadostesouro.q3 ||
+      //   cotacao.rentabilidadeAnual >= dadostesouro.max
+      // ) {
+      //   message = `*Título:* ${cotacao.titulo}\n*Preço unitário:* ${cotacao.precoUnitario}\n*Investimento mínimo:* ${cotacao.investimentoMinimo}\n*Rentabilidade anual:* ${cotacao.rentabilidadeAnual}%\n*Vencimento:* ${cotacao.vencimento}\n\n`;
+      //   message += `*Mínimo:* ${dadostesouro.min}\n*1º quartil:* ${dadostesouro.q1}\n*Mediana:* ${dadostesouro.median}\n*3º quartil:* ${dadostesouro.q3}\n*Máximo:* ${dadostesouro.max}\n*Média:* ${dadostesouro.mean}\n*Desvio padrão:* ${dadostesouro.stdev}\n\n`;
+      //   message += "😀 *J4 - COMPRA ÓTIMA*\n\n";
+      // }
 
       if (message !== "") {
         await ctx.replyWithMarkdown(message, keyboard);
