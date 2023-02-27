bf94ef2f8a61445019beb6933107901dd66ef150
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2023-02-26 23:30:12 -0300

    bugfix

--- downloadTesouro.js
+++ downloadTesouro.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const URL_API = 'https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json';
 const URL_FILE_TESOURO = 'https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv';
 
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 // Baixa o arquivo JSON da API
 https.get(URL_API, (res) => {
   let data = '';
