b9f0e89a1d4329ed4d89023f7500293edcb690a0
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2024-08-21 09:44:53 -0300

    Update downloadTesouro.js

--- downloadTesouro.js
+++ downloadTesouro.js
@@ -9,7 +9,7 @@ const URL_API =
 const URL_FILE_TESOURO =
   "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv";
 
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 async function downloadArquivo(arquivo, url) {
   console.log(`Iniciando download do arquivo ${arquivo}...`);
