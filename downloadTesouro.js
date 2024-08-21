839c3e9331d66c9bbebf2c7596b18d774e31d7a8
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2024-08-21 11:01:14 -0300

    update

--- downloadTesouro.js
+++ downloadTesouro.js
@@ -1,24 +1,56 @@
 const axios = require("axios");
 const fs = require("fs");
+const puppeteer = require("puppeteer");
 
 const arquivoJson = "tesouro.json";
 const arquivoCsv = "PrecoTaxaTesouroDireto.csv";
-
-// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 const URL_API =
   "https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json";
 const URL_FILE_TESOURO =
   "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv";
 
+async function downloadJsonViaPuppeteer(url, arquivo) {
+  let browser;
+  try {
+    console.log(`Iniciando download do arquivo ${arquivo} via Puppeteer...`);
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+
+    // Configurar o User-Agent e outros cabeçalhos para emular um navegador real
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    );
+    // Navegar diretamente para a URL da API
+    const response = await page.goto(url, {
+      waitUntil: "networkidle2", // Espera até que a rede esteja ociosa
+      timeout: 120000, // Timeout ajustado para 120 segundos
+    });
+
+    if (response.ok()) {
+      const jsonContent = await response.text();
+      fs.writeFileSync(arquivo, jsonContent);
+      console.log(`${arquivo} salvo localmente.`);
+    } else {
+      throw new Error(`Resposta da API com status: ${response.status()}`);
+    }
+  } catch (error) {
+    console.error(`Erro ao baixar o arquivo ${arquivo}: ${error.message}`);
+    throw error;
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+}
+
 async function downloadArquivo(arquivo, url) {
   console.log(`Iniciando download do arquivo ${arquivo}...`);
   try {
     const response = await axios.get(url, {
       responseType: "stream",
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-      }
     });
 
     console.log(`Download do ${arquivo} concluído.`);
@@ -36,7 +68,7 @@ async function downloadArquivo(arquivo, url) {
       });
     });
   } catch (error) {
-    console.error(`Erro ao baixar o arquivo ${arquivo}:`, error.message);
+    console.error(`Erro ao baixar o arquivo ${arquivo}: ${error.message}`);
     throw error;
   }
 }
@@ -44,7 +76,7 @@ async function downloadArquivo(arquivo, url) {
 (async () => {
   try {
     await Promise.all([
-      downloadArquivo(arquivoJson, URL_API),
+      downloadJsonViaPuppeteer(URL_API, arquivoJson),
       downloadArquivo(arquivoCsv, URL_FILE_TESOURO),
     ]);
     console.log("Todos os downloads foram concluídos.");
